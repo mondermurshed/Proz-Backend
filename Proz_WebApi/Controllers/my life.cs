ba691dbb34985c109a823b2a704b2f97705da320
monder <monder@DESKTOP-FEH4U92>
2025-02-28 23:37:07 +0300

    we added a new controller + we modified the program.cs file

---
+++ Proz_WebApi/Controllers/my life.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proz_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class my_life : ControllerBase
+    {
+    }
+}
--- Proz_WebApi/Program.cs
+++ Proz_WebApi/Program.cs
@@ -22,4 +22,3 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
